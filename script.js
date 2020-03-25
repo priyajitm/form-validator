a8e1bc1ca2b6aa200cb3c413ae8981fafd601f3d
Priyajit Mukherjee <priyajit@Priyajits-iMac.local>
2020-03-26 01:19:36 +0530

    validation bugs

--- script.js
+++ script.js
@@ -20,20 +20,58 @@ function showSuccess(input) {
 }
 
 //Check email is valid
-function isValidEmail(email) {
+function checkEmail(input) {
   const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(String(email).toLowerCase());
+  if (re.test(input.value.trim())) {
+    showSuccess(input);
+  } else {
+    showError(input, 'Email is not valid');
+  }
 }
 
 // Check required fields
 function checkRequired(inputarr) {
   inputarr.forEach(function(input) {
     if (input.value.trim() === '') {
-      showError(input, `${getFieldName(input)} is required`);
+      if (input.id === 'password2') {
+        showError(input, `Confirm Password is required`);
+      } else {
+        showError(input, `${getFieldName(input)} is required`);
+      }
     } else {
-      showSuccess(input);
+      // showSuccess(input);
+      checkLength(username, 3, 5);
+      checkLength(password, 6, 25);
+      checkEmail(email);
+      checkPasswordMatch(password, password2);
     }
   });
+}
+
+// Check input length
+function checkLength(input, min, max) {
+  if (input.value.length < min) {
+    showError(
+      input,
+      `${getFieldName(input)} must be atleast ${min} characters`
+    );
+  } else if (input.value.length > max) {
+    showError(
+      input,
+      `${getFieldName(input)} must be less than ${max} characters`
+    );
+  } else {
+    showSuccess(input);
+  }
+}
+
+//Check passwords match
+function checkPasswordMatch(input1, input2) {
+  if (input1.value !== input2.value) {
+    showError(input2, 'Passwords do not match');
+  } else {
+    showSuccess(input2);
+  }
 }
 
 // Get fieldname
@@ -44,5 +82,8 @@ function getFieldName(input) {
 // Event Listeners
 form.addEventListener('submit', function(e) {
   e.preventDefault();
+
   checkRequired([username, email, password, password2]);
+  //   checkLength(username, 3, 5);
+  //   checkLength(password, 6, 25);
 });
